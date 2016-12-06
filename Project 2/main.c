d0f02702a111f0718399fcf255f918dcf68ea17a
Jesse Huang <jessehuang2222@gmail.com>
2016-12-07 05:10:50 +0800

    Finish all functions

--- Project 2/main.c
+++ Project 2/main.c
@@ -4,19 +4,17 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void* DEBUG_recvall(void *arg);
 int create_connection();
-int do_action(int sock, const char *tag, const char* content);
+int do_action(int sock, const char *tag, char *content);
 int login(int sock, const char *uname, const char *passwd);
-int change_board(int sock, char *board_name);
-int set_title(int sock, char *title);
-int post(int sock, char *post);
-int logout(int sock);
+int change_board(int sock, const char *board_name);
+int set_title(int sock, const char *title);
+int post(int sock, const char *content);
+void logout(int sock);
 
 int create_connection() {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -25,11 +23,11 @@ int create_connection() {
         exit(1);
     }
 
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+    struct addrinfo hints = {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+        .ai_flags=AI_PASSIVE
+    };
 
     struct addrinfo *srvinf;
     if(getaddrinfo("ptt.cc", "telnet", &hints, &srvinf) < 0) {
@@ -46,84 +44,87 @@ int create_connection() {
     return sock;
 }
 
-int do_action(int sock, const char *tag, const char* content) {
+int do_action(int sock, const char *tag, char* content) {
+    static char username[32], password[32];
+    if(!strncmp(tag, "ID", 2)) {
+        strncpy(username, content, 32);
+    } else if(!strncmp(tag, "PASS", 4)) {
+        strncpy(password, content, 32);
+        login(sock, username, password);
+    } else if(!strncmp(tag, "BOARD", 5)) {
+        change_board(sock, content);
+    } else if(!strncmp(tag, "P", 1)) {
+        set_title(sock, content);
+    } else if(!strncmp(tag, "CONTENT", 7)) {
+        char *p = content;
+        while((p = strchr(p, '\n')))
+            *p = '\r';
+        post(sock, content);
+    }
+    sleep(1);
+    return 0;
 }
 
 int login(int sock, const char *uname, const char *passwd) {
-    printf("login with uname='%s', passwd='%s'\n", uname, passwd);
-    write(sock, "\b\b\b\b\b\b\b\b\b\b", 10);
-    write(sock, (void *)uname, strlen(uname));
-    write(sock, (void *)",\r", 2);
-    write(sock, (void *)passwd, strlen(passwd));
-    write(sock, (void *)"\r", 1);
-    sleep(1);
-    write(sock, (void *)"\03\03\03\03\03\03\03\03\03\03", 10);
-    sleep(1);
-    
+    dprintf(sock, "\b\b\b\b\b\b\b\b\b\b");
+    // username -> , -> \r -> password -> \r
+    dprintf(sock, "%s,\r%s\r", uname, passwd);
+    dprintf(sock, "\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03");
     return 0;
 }
 
-void* DEBUG_recvall(void *arg) {
-    int sock = *(int *)arg;
-    char buf[4096];
-    for(;;) {
-        read(sock, (void *)buf, sizeof(buf));
-        printf("%s", buf);
-        
-    }
+int change_board(int sock, const char *board_name) {
+    dprintf(sock, "\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03");
+    // s -> board_name -> \r
+    dprintf(sock, "s%s\r", board_name);
+    dprintf(sock, "\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03");
     return 0;
 }
 
-int change_board(int sock, char *board_name) {
-    write(sock, (void *)"\03\03\03\03\03\03\03\03\03\03", 10);
-    write(sock, (void *)"s", 1);
-    write(sock, (void *)board_name, strlen(board_name));
-    write(sock, (void *)"\r", 1);
-    sleep(1);
-    write(sock, (void *)"\03\03\03\03\03\03\03\03\03\03", 10);
-    sleep(1);
+int set_title(int sock, const char *title) {
+    dprintf(sock, "\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03");
+    // ^P -> \r -> title -> \r
+    dprintf(sock, "\x10\r%s\r", title);
     return 0;
 }
 
-int set_title(int sock, char *title) {
+int post(int sock, const char *content) {
+    dprintf(sock, "\b\b\b\b\b\b\b\b\b\b");
+    // content -> ^x -> s -> enter -> etner
+    dprintf(sock, "%s\x18s\r\r", content);
     return 0;
 }
 
-int set_content(int sock, char *post) {
-    return 0;
-}
-
-int logout(int sock) {
-    write(sock, "", 0);
+void logout(int sock) {
+    // left key * 10
+    dprintf(sock, "\033[D\033[D\033[D\033[D\033[D\033[D\033[D\033[D\033[D\033[D");
     sleep(1);
-    write(sock, "", 0);
+    // right key -> y -> \r
+    dprintf(sock, "\033[Cy\r");
     sleep(1);
-    return 0;
+    exit(0);
 }
 
 int main(void) {
-//    int conn = create_connection();
+    int conn = create_connection();
     FILE *fp = fopen("P_input.txt", "r");
     if(fp == NULL) {
         fprintf(stderr, "P_input.txt open failed\n");
-        exit(-1);
+        exit(1);
     }
-//    pthread_t tid;
-//    pthread_create(&tid, NULL, DEBUG_recvall, (void *)&conn);
-    
     
     char content[4096];
     char buf[8192];
-
-//    login(conn, username, password);
-    
-    int closed = 1;
     char tag[32], close_tag[32];
     char *buf_p;
+
+    int closed = 1;
     for(;;) {
         if(closed) {
             fgets(buf, 4096, fp);
-            if(!strncmp(buf, "<EXIT>", 6)) { return 0;}
+            if(!strncmp(buf, "<EXIT>", 6))
+                logout(conn);
+
             sscanf(buf, "<%[^>]", tag);
             snprintf(close_tag, 32, "</%s>", tag);
 
@@ -133,10 +134,9 @@ int main(void) {
         } else {
             char *end_tag_p;
             if((end_tag_p = strstr(buf_p, close_tag))) {
-                content[strlen(content) + (end_tag_p - buf_p)] = '\0';
                 strncat(content, buf_p, end_tag_p - buf_p);
+                do_action(conn, tag, content);
                 closed = 1;
-                printf("TAG = %s, CONTENT = %s\n", tag, content);
             } else {
                 strcat(content, buf_p);
                 fgets(buf, 4096, fp);
